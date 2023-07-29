9041c9d131e4ee90d3c32cc782db0a5628378a01
Ganstex <104472291+ganstex@users.noreply.github.com>
2023-07-29 18:29:26 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+# Configura el prefijo para los comandos del bot
+prefix = "!"  # Puedes cambiar el prefijo a lo que desees
+
+# Crea una instancia del bot con el prefijo definido
+bot = commands.Bot(command_prefix=prefix)
+
+# Evento que se ejecuta cuando el bot está listo y conectado a Discord
+@bot.event
+async def on_ready():
+    print(f"¡{bot.user.name} está listo para ser utilizado!")
+
+# Comando simple
+@bot.command()
+async def hola(ctx):
+    await ctx.send(f"Hola, {ctx.author.mention}!")
+
+# Comando para sumar dos números
+@bot.command()
+async def sumar(ctx, num1: int, num2: int):
+    resultado = num1 + num2
+    await ctx.send(f"El resultado de {num1} + {num2} es {resultado}.")
+
+# Comando para mostrar información sobre el bot
+@bot.command()
+async def informacion(ctx):
+    embed = discord.Embed(title="Información del Bot",
+                          description="Este es un bot de ejemplo creado con discord.py",
+                          color=discord.Color.blue())
+    embed.add_field(name="Autor", value="Tu Nombre Aquí")
+    embed.add_field(name="Versión", value="1.0")
+    await ctx.send(embed=embed)
+
+# Ejecutar el bot con el token del bot que obtuviste desde el sitio web de desarrolladores de Discord
+bot.run
